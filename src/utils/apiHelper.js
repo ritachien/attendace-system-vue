f36af28f0653d00d66fd36609e4c5a74553c88f8
Rita Chien <ritachien929@gmail.com>
2022-12-27 23:56:21 +0800

    feat: re-arange validateStatus config to get error msg from API

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -4,4 +4,7 @@ const baseURL = 'http://localhost:3000/api'
 
 export const apiHelper = axios.create({
   baseURL,
+  validateStatus: function (status) {
+    return status >= 200 && status < 500
+  }
 })
