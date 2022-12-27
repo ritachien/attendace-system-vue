dc9def4816de7c8b498f49ba28e0211731a1691d
Rita Chien <ritachien929@gmail.com>
2022-12-27 23:57:09 +0800

    feat: adjust Toast width

--- src/utils/swal.js
+++ src/utils/swal.js
@@ -2,8 +2,9 @@ import Swal from 'sweetalert2'
 
 export const Toast = Swal.mixin({
   toast: true,
+  width: '25rem',
   position: 'top-end',
-  showCancelButton: false,
+  showConfirmButton: false,
   timer: 3000,
   timerProgressBar: true
 })
