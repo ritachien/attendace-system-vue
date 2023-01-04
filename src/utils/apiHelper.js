f5a5eb462eafd3df82f9ac93d12b9ad816b3c3f7
Rita Chien <ritachien929@gmail.com>
2023-01-04 17:30:26 +0800

    feat: set production API url to connect backend

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3000/api'
+// const developmentURL = 'http://localhost:3000/api'
+const productionURL = 'https://attendance-system-express-production.up.railway.app/api'
 
 export const apiHelper = axios.create({
-  baseURL,
+  baseURL: productionURL,
   validateStatus: function (status) {
     return status >= 200 && status < 500
   }
