0f197506f80af596d77da670398ca1450c17115a
Rita Chien <ritachien929@gmail.com>
2023-01-05 12:40:23 +0800

    feat: add googleMap.vue to show current position

--- src/utils/apiHelper.js
+++ src/utils/apiHelper.js
@@ -1,10 +1,13 @@
 import axios from 'axios'
 
-// const developmentURL = 'http://localhost:3000/api'
-const productionURL = 'https://attendance-system-express-production.up.railway.app/api'
+// for dev use
+// const baseURL = 'http://localhost:3000/api'
+
+// for production use
+const baseURL = 'https://attendance-system-express-production.up.railway.app/api'
 
 export const apiHelper = axios.create({
-  baseURL: productionURL,
+  baseURL,
   validateStatus: function (status) {
     return status >= 200 && status < 500
   }
