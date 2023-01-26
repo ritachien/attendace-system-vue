629cc3c7deb0d2e4aecabc9427eec0cbc8225a37
Rita Chien <ritachien929@gmail.com>
2023-01-26 10:58:48 +0800

    feat: add CheckBox component

---
+++ src/components/formItems/CheckBox.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- <n-form-item> -->
+
+  <n-checkbox
+    :id="props.field"
+    v-model:checked="input"
+    type="checkbox"
+    size="large"
+    @update:checked="updateInputValue"
+  >
+    {{ props.labelText }}
+  </n-checkbox>
+
+  <!-- </n-form-item> -->
+</template>
+<script setup>
+import { ref } from 'vue'
+import { NCheckbox } from 'naive-ui'
+
+const emits = defineEmits(['updateInput'])
+const props = defineProps({
+  labelText: {
+    type: String,
+    default: '',
+  },
+  field: {
+    type: String,
+    default: '',
+  },
+  value: {
+    type: Boolean,
+    default: false,
+  }
+})
+
+const input = ref(null)
+
+function updateInputValue () {
+  emits('updateInput', {
+    field: props.field,
+    data: input.value,
+  })
+}
+</script>
