5d5d476cb366a958417bdbdf4bb8a300b05c7687
Rita Chien <ritachien929@gmail.com>
2023-01-26 11:08:43 +0800

    feat: add SubmitButton component

---
+++ src/components/formItems/SubmitButton.vue
@@ -0,0 +1,36 @@
+<template>
+  <n-button
+    strong
+    secondary
+    block
+    size="large"
+    type="info"
+    class="submitButton"
+    @click="props.clickEvent"
+    attr-type="submit"
+  >
+    {{ props.buttonText }}
+  </n-button>
+</template>
+
+<script setup>
+import { NButton } from 'naive-ui'
+
+const props = defineProps({
+  clickEvent: {
+    type: Function,
+    default: () => {},
+  },
+  buttonText: {
+    type: String,
+    default: '',
+  }
+})
+</script>
+
+<style scoped>
+.submitButton {
+  margin-top: 2vh;
+  font-size: large;
+}
+</style>
