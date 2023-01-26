fd391ee267c4cb96759eea128c9783c204c165c5
Rita Chien <ritachien929@gmail.com>
2023-01-26 12:12:49 +0800

    feat: add RecordCard component

---
+++ src/components/RecordCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <n-card
+    class="card"
+    size="small"
+    bordered
+    :embedded="true"
+  >
+    {{ props.time }}
+  </n-card>
+</template>
+
+<script setup>
+import { NCard } from 'naive-ui'
+
+const props = defineProps({
+  time: {
+    type: String,
+    default: null,
+  }
+})
+</script>
+
+<style scoped>
+.card {
+  min-width: 150px;
+  background-color: #fae3d9;
+  border: 1px solid #ffb6b9;
+}
+</style>
