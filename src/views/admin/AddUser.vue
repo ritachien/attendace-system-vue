b500ab2b470189a8ba948deb62e94c3da3af8da6
Rita Chien <ritachien929@gmail.com>
2023-01-26 12:04:09 +0800

    refactor: replace input elements by InputItem component in AddUser

--- src/views/admin/AddUser.vue
+++ src/views/admin/AddUser.vue
@@ -1,139 +1,161 @@
 <template>
-  <div class="container py-5">
-    <form
-      class="w-100"
-      @submit.prevent.stop="handleSubmit"
-    >
-      <div class="text-center my-4">
-        <h1 class="h3 mb-4 font-weight-normal">
-          新增使用者
-        </h1>
-      </div>
+  <main>
+    <h1>新增使用者</h1>
 
-      <div class="form-label-group my-4">
-        <label for="account">Account</label>
-        <input
-          id="account"
-          v-model.trim="userData.account"
-          name="account"
-          type="text"
-          class="form-control"
-          placeholder="使用者帳號"
-          required
-          autofocus
-        >
-      </div>
+    <n-form :model="userData">
 
-      <div class="form-label-group my-4">
-        <label for="name">Name</label>
-        <input
-          id="name"
-          v-model.trim="userData.name"
-          placeholder="使用者姓名"
-          name="name"
-          type="text"
-          class="form-control"
-          required
-        >
-      </div>
+      <!-- 帳號 -->
+      <InputItem
+        label="Account"
+        field="account"
+        type="text"
+        @updateInput="dataEmit"
+        :value="userData.account"
+        :required="formRequired.account"
+        :isValidInput="isValidAccount"
+        :feedback="validationFeedback.account"
+        placeholder="Enter account here..."
+        :key="`account${submitTimes}`"
+      />
 
-      <div class="form-label-group my-4">
-        <label for="email">Email</label>
-        <input
-          id="email"
-          v-model.trim="userData.email"
-          placeholder="使用者 email"
-          name="email"
-          type="email"
-          class="form-control"
-        >
-      </div>
+      <!-- Name -->
+      <InputItem
+        label="Name"
+        field="name"
+        type="text"
+        @updateInput="dataEmit"
+        :value="userData.name"
+        :required="formRequired.name"
+        :isValidInput="isValidName"
+        :feedback="validationFeedback.name"
+        placeholder="Enter name here..."
+        :key="`name${submitTimes}`"
+      />
 
-      <div class="form-label-group my-4">
-        <label for="password">Password</label>
-        <input
-          id="password"
-          name="password"
-          type="text"
-          class="form-control"
-          placeholder="預設密碼: titaner"
-          disabled
-        >
-      </div>
+      <!-- Email -->
+      <InputItem
+        label="Email"
+        field="email"
+        type="email"
+        @updateInput="dataEmit"
+        :value="userData.email"
+        :required="formRequired.email"
+        :isValidInput="isValidEmail"
+        :feedback="validationFeedback.email"
+        placeholder="Enter email here..."
+        :key="`email${submitTimes}`"
+      />
 
-      <div class="row mt-5 px-3">
-        <n-button
-          strong
-          secondary
-          type="info"
-          class="clock-button"
-          @click="handleSubmit"
-          attr-type="submit"
-          :disabled="isNotReadyForSubmit || isProcessing"
-        >
-          Submit
-        </n-button>
-      </div>
-    </form>
-  </div>
+      <!-- 密碼 -->
+      <InputItem
+        label="Password"
+        field="password"
+        type="password"
+        placeholder="預設為 'titaner'"
+        :disabled="true"
+      />
+
+      <SubmitButton
+        :clickEvent="handleSubmit"
+        :disabled="isInputAllFilled || isProcessing"
+        :loading="isProcessing"
+        buttonText="Save"
+      />
+
+    </n-form>
+  </main>
 </template>
 
 <script setup>
 import { reactive, computed, ref } from 'vue'
-import { NButton } from 'naive-ui'
+import { NForm } from 'naive-ui'
 
+import InputItem from '../../components/formItems/InputItem.vue'
+import SubmitButton from '../../components/formItems/SubmitButton.vue'
+
+import { useInputValidation } from '../../composable/useInputValidation'
 import { popErrMsg, popConfirmMsg } from '../../utils/swal'
 import adminAPI from '../../apis/admin'
 
 // 紀錄本次改動內容
 const userData = reactive({
-  account: '',
-  email: '',
-  name: '',
+  account: null,
+  email: null,
+  name: null,
 })
+const submitTimes = ref(0)
 const isProcessing = ref(false)
-const isNotReadyForSubmit = computed(() => {
-  // 如果任一欄位 account、email、password 任一欄位有變動
+const isInputAllFilled = computed(() => {
+  // 檢查所有欄位是否已填寫
   return userData.account && userData.name && userData.email
     ? false
     : true
 })
 
+// input validation
+const formRequired = reactive({
+  account: true,
+  email: true,
+  name: true,
+})
+const validationFeedback = reactive({
+  account: null,
+  name: null,
+  email: null,
+})
+const isValidAccount = computed(() => useInputValidation({
+  field: 'account',
+  data: userData.account,
+  required: formRequired.account,
+}))
+const isValidEmail = computed(() => useInputValidation({
+  field: 'email',
+  data: userData.email,
+  required: formRequired.email,
+}))
+const isValidName = computed(() => useInputValidation({
+  field: 'name',
+  data: userData.name,
+  required: formRequired.name,
+}))
+
+// Update emit datas
+function dataEmit ({ field, data }) {
+  if (data === undefined) return
+  return userData[field] = !data ? null : data
+}
+
 // 表單發送
 async function handleSubmit () {
   try {
     // 請求處理未完成時 disable submit button
+    let errors = false
     isProcessing.value = true
 
-    // 檢查 user Input
-    const accountReg = /^\w{5,12}$/
-    const emailReg = /^[-\w.]+@([-\w]+\.)+[-\w]{2,4}$/
-    const nameReg = /.{1,30}/
-
-    if (!accountReg.test(userData.account)) {
-      isProcessing.value = false
-      return popErrMsg('帳號須由 5~12 碼英數字組成!')
+    // 檢查所有的 input
+    if (!isValidAccount.value) {
+      validationFeedback.account = '帳號須由 5~12 碼英數字組成!'
+      errors = true
     }
 
-    if (!emailReg.test(userData.email)) {
-      isProcessing.value = false
-      return popErrMsg('請使用正確 email 格式!')
+    if (!isValidEmail.value) {
+      validationFeedback.email = '請使用正確 email 格式!'
+      errors = true
     }
 
-    if (!nameReg.test(userData.name)) {
-      isProcessing.value = false
-      return popErrMsg('名字為必填，並介於 1~30 字間。')
+    if (!isValidName.value) {
+      validationFeedback.name = '名字為必填，並介於 1~30 字間。'
+      errors = true
     }
+
+    // 如果有任一錯誤，提示錯誤訊息並終止 submit
+    if (errors) return isProcessing.value = false
 
     // 發出 API 請求
     const { data: { status, message, user } } = await adminAPI.addUser(userData)
 
-    if (status === 'error') {
-      isProcessing.value = false
-      return popErrMsg(message)
-    }
+    if (status === 'error') throw new Error(message)
 
-    isProcessing.value = false
     return popConfirmMsg({
       icon: 'success',
       msg: '新建成功',
@@ -145,8 +167,9 @@ async function handleSubmit () {
       `
     })
   } catch (err) {
-    isProcessing.value = false
     return popErrMsg(err)
+  } finally {
+    isProcessing.value = false
   }
 }
 
@@ -154,8 +177,14 @@ async function handleSubmit () {
 
 
 <style scoped>
-form {
-  margin: 0 auto;
+main {
+  margin: 10vh auto;
+  padding: 0 5vw;
+  min-width: 350px;
   max-width: 600px;
+}
+
+h1 {
+  text-align: center;
 }
 </style>
